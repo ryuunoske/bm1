a2824720eca703c953f87b780945378849772fea
ryuunoske <ryuunoske@dsmc.co.in>
2015-08-22 14:53:02 +0530

    Init loctrac1

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150820113607) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.string   "address"
     t.float    "latitude"
